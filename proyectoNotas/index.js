a12e642a6a01eb32b1152306ded042f4a421d8ee
Cristina Froján <frojancorralc@gmail.com>
2019-11-11 23:23:58 +0100

    Actualización

---
+++ proyectoNotas/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+console.log("conexión establecida");
+
+const query = "SELECT `id` FROM `notes`";
+
+function tratarResultadoQuery(err, results) {
+  if (err) {
+    console.error("ERROR!!!!!", err);
+    process.exit(-1);
+  }
+  console.log(results);
+  connection.close();
+}
+
+connection.query(query, tratarResultadoQuery);
--- proyectoNotas/www/js/main.js
+++ proyectoNotas/www/js/main.js
@@ -1,33 +1,103 @@
-const menu = document.getElementById("menu");
-const content = document.getElementById("content");
-const noteSection = document.getElementById("notes");
-const notes = noteSection.querySelectorAll("li");
+// const menu = document.getElementById("menu");
+// const content = document.getElementById("content");
+// const noteSection = document.getElementById("notes");
+// const notes = noteSection.querySelectorAll("li");
 
-//Functions
-const toggleMenu = e => {
-  content.classList.toggle("menu");
-};
+// //Functions
+// const toggleMenu = e => {
+//   content.classList.toggle("menu");
+//   content.classList.remove("note");
+//   unselectAllNotes();
+// };
 
-const hideMenu = e => {
-  content.classList.remove("menu");
-};
+// // const hideMenu = e => {
+// //   content.classList.remove("menu");
+// // };
 
-const selectNote = e => {
-  if (content.classList.contains("menu")) return;
+// const selectNote = e => {
+//   if (content.classList.contains("menu")) {
+//     content.classList.remove("menu");
+//     return;
+//   }
 
-  const target = e.currentTarget;
+//   unselectAllNotes();
 
+//   const li = e.currentTarget;
+//   li.classList.add("note");
+
+//   // for (const note of notes) {
+//   //   note.classList.remove("note");
+//   // }
+
+//   // target.classList.add("selected");
+// };
+
+// function unselectAllNotes() {
+//   for (const note of notes) {
+//     note.classList.remove("selected");
+//   }
+// }
+
+// function unselecNote(e) {
+//   e.preventDefault();
+//   unselectAllNotes();
+
+//   content.classList.remove("note");
+// }
+// //Event listeners
+// menu.addEventListener("click", toggleMenu);
+// // noteSection.addEventListener("click", hideMenu);
+
+// for (const note of notes) {
+//   note.addEventListener("click", selectNote);
+// }
+// back.addEventListener("click", unselecNote);
+
+const content = document.querySelector("section#content");
+const menu = document.querySelector("button#menu");
+const back = document.querySelector("article#note form button");
+const notes = document.querySelectorAll("section#notes ul li");
+
+function unselectAllNotes() {
   for (const note of notes) {
     note.classList.remove("selected");
   }
+}
 
-  target.classList.add("selected");
-};
+function toggleMenu() {
+  content.classList.toggle("menu-visible");
+  content.classList.remove("note-visible");
+  unselectAllNotes();
+}
 
-//Event listeners
+function selectNote(e) {
+  //Si está el menú visible oculta el menú en lugar de seleccionar nota
+  if (content.classList.contains("menu-visible")) {
+    //valdría ejecutar toggleMenu()
+    content.classList.remove("menu-visible");
+    return;
+  }
+  // quitar la clase selected de todos los li
+  unselectAllNotes();
+
+  // añadir la clase selected al li que cliqué
+  const li = e.currentTarget;
+  li.classList.add("selected");
+  content.classList.add("note-visible");
+}
+
+function unselectNote(e) {
+  e.preventDefault();
+  unselectAllNotes();
+
+  content.classList.remove("note-visible");
+}
+
+// Event listeners
 menu.addEventListener("click", toggleMenu);
-noteSection.addEventListener("click", hideMenu);
 
 for (const note of notes) {
   note.addEventListener("click", selectNote);
 }
+
+back.addEventListener("click", unselectNote);
